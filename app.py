f8d7d455bb0faed54a670071defe6dedd16da7d0
Drew Anello <drew.anello@gmail.com>
2022-12-28 19:55:00 +0000

    adds client events

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import discord
 import os
 
-# client = discord Client()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+@client.event()
+async def on_message(message):
+  if message.author == client.user:
+    return
+
