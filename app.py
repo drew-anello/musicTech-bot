84f03e5bd2f150c9aac1e938d37f86e9fe8cb22f
Drew Anello <drew.anello@gmail.com>
2022-12-30 02:20:23 +0000

    stopping point

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ import os
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+
+
 @client.event()
 async def on_message(message):
   if message.author == client.user:
     return
 
+
+# pickup here
